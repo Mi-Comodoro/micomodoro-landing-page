ae4b81ee90101d1c68d402330f99cf8da6410f22
Miguel Alvarez <ing.migueljalvarez@gmail.com>
2026-05-15 21:09:28 -0500

    feat(landing): add centralized CTA URL constants

---
+++ src/config/urls.ts
@@ -0,0 +1,6 @@
+export const APP_URL = 'https://app.micomodoro.com'
+
+export const CTA_URLS = {
+  free: `${APP_URL}/?plan=free&ref=landing`,
+  pro: `${APP_URL}/?plan=pro&ref=landing`,
+} as const
